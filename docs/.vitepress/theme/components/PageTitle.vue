671fae84fbc1ccad36b09577e1359c89fad84d77
Yuki Soffd <654952566@qq.com>
2026-08-14 22:46:37 +0800

    docs: 基底

--- docs/.vitepress/theme/components/PageTitle.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { useData } from 'vitepress'
-
-const { page } = useData()
-</script>
-
-<template>
-  <h1 v-if="page.title" class="page-gradient-title">{{ page.title }}</h1>
-</template>
